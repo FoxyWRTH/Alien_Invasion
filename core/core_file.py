e9fbf520db4bbd7964af0eba55f58130f6b1ea5c
Fox <f0xxxywtf@gmail.com>
2022-12-01 19:16:27 +0300

    Почти закончил. Игры на питоне — не моё… Да и в целом Гейм дев.

--- core/core_file.py
+++ core/core_file.py
@@ -16,18 +16,18 @@ def run():
     bg_color = (0, 0, 0)  # Заливка цветом.
     player_ship = PlayerShip(screen)
     bullet_list = Group()
-    enemy = Group()
-    button_controls.create_army(screen, enemy)
+    enemies = Group()
+    button_controls.create_army(screen, enemies)
     statis = Statis()
 
     while True:  # Бесконечный цикл игры. Обработка действий игрока.
         button_controls.events(screen, player_ship, bullet_list)
         player_ship.update_ship()  # Отрисовка корабля.
-        button_controls.update(bg_color, screen, player_ship, enemy,
+        button_controls.update(bg_color, screen, player_ship, enemies,
                                bullet_list)
-        button_controls.update_bullets(screen, enemy, bullet_list)
+        button_controls.update_bullets(screen, enemies, bullet_list)
         button_controls.update_enemies(statis, screen,
-                                       player_ship, enemy, bullet_list)
+                                       player_ship, enemies, bullet_list)
 
 
 if __name__ == '__main__':
